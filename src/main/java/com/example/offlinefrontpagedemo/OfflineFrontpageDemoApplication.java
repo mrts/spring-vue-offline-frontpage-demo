312f80894d9a20992f11c339ccb3d061161560df
Mart Sõmermaa <mart.somermaa@gmail.com>
2017-06-21 22:45:34 +0300

    Project template from Spring Initializr

---
+++ src/main/java/com/example/offlinefrontpagedemo/OfflineFrontpageDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.offlinefrontpagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflineFrontpageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfflineFrontpageDemoApplication.class, args);
+	}
+}
